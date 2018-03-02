7a639079438bbefb472f854e203192b42bf0d510
bathulah <bathulahmahir@gmail.com>
2018-03-02 12:43:23 +0800

    add autoload file

---
+++ src/OAuth2ApiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Batmahir\OAuth2;
+
+use Illuminate\Support\ServiceProvider;
+
+class OAuth2ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
